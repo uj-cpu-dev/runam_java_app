8d232bb0b305707322fda35d87b5631152064277
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2025-06-22 18:05:49 +0100

    json

--- src/main/java/rum_am_app/run_am/controller/UserController.java
+++ src/main/java/rum_am_app/run_am/controller/UserController.java
@@ -2,6 +2,7 @@ package rum_am_app.run_am.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,7 @@ public class UserController {
 
     @PostMapping("/signup")
     public ResponseEntity<User> signup(@Valid @RequestBody UserSignupRequest request) {
-        User created = userService.register(request);
-        return ResponseEntity.ok(created);
+        User newUser = userService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 }
